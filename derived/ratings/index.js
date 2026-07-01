d5d777588bf5056b7030f5ab181f25db74453dce
davidmarsden <dmarsd@gmail.com>
2026-07-01 20:10:31 +0100

    Add rating formula exports

---
+++ derived/ratings/index.js
@@ -0,0 +1,4 @@
+export { abilityFromMarketValue, ABILITY_ANCHORS } from "./ability.js";
+export { formFromOutput, effectiveMatchRating, LEAGUE_WEIGHTS } from "./form.js";
+export { potentialBand } from "./potential.js";
+export { capsScoreFromCaps, reputationFromCareerStature } from "./reputation.js";
