57f6bea696913c8f133bfa21cdb49f45fd5864c9
davidmarsden <dmarsd@gmail.com>
2026-07-05 20:58:58 +0100

    Add widened Transfermarkt import scope

---
+++ scripts/transfermarkt-scope.js
@@ -0,0 +1,98 @@
+export const TRANSFERMARKT_WIDE_COMPETITION_CODES = [
+  // Big five / core calibration leagues
+  "GB1", // Premier League
+  "ES1", // LaLiga
+  "IT1", // Serie A
+  "L1", // Bundesliga
+  "FR1", // Ligue 1
+
+  // Useful TBG calibration and transfer-market context
+  "GB2", // Championship
+  "PO1", // Liga Portugal
+  "NL1", // Eredivisie
+  "TR1", // Süper Lig
+  "BE1", // Jupiler Pro League
+  "SA1", // Saudi Pro League
+  "MLS1" // Major League Soccer
+];
+
+export const TRANSFERMARKT_GENERIC_START_URLS = [
+  "https://www.transfermarkt.com/premier-league/startseite/wettbewerb/GB1",
+  "https://www.transfermarkt.com/laliga/startseite/wettbewerb/ES1",
+  "https://www.transfermarkt.com/serie-a/startseite/wettbewerb/IT1",
+  "https://www.transfermarkt.com/bundesliga/startseite/wettbewerb/L1",
+  "https://www.transfermarkt.com/ligue-1/startseite/wettbewerb/FR1",
+  "https://www.transfermarkt.com/championship/startseite/wettbewerb/GB2",
+  "https://www.transfermarkt.com/liga-portugal/startseite/wettbewerb/PO1",
+  "https://www.transfermarkt.com/eredivisie/startseite/wettbewerb/NL1",
+  "https://www.transfermarkt.com/super-lig/startseite/wettbewerb/TR1",
+  "https://www.transfermarkt.com/jupiler-pro-league/startseite/wettbewerb/BE1",
+  "https://www.transfermarkt.com/saudi-pro-league/startseite/wettbewerb/SA1",
+  "https://www.transfermarkt.com/major-league-soccer/startseite/wettbewerb/MLS1"
+];
+
+export const GOLD_STANDARD_RESCUE_PLAYERS = [
+  "Kylian Mbappe",
+  "Vinicius Junior",
+  "Jude Bellingham",
+  "Lamine Yamal",
+  "Jamal Musiala",
+  "Robert Lewandowski",
+  "Harry Kane",
+  "Kevin De Bruyne",
+  "Toni Kroos",
+  "Luka Modric",
+  "Manuel Neuer",
+  "Thibaut Courtois",
+  "Ruben Dias",
+  "Achraf Hakimi",
+  "Federico Valverde",
+  "Pedri",
+  "Rafael Leao",
+  "Lautaro Martinez",
+  "Victor Osimhen",
+  "Khvicha Kvaratskhelia",
+  "Ousmane Dembele",
+  "Paulo Dybala",
+  "Romelu Lukaku",
+  "Alejandro Grimaldo",
+  "Nico Williams",
+  "Jarrod Bowen",
+  "Marc Guehi",
+  "Joao Pedro",
+  "Jean-Ricner Bellegarde",
+  "Serhou Guirassy",
+  "Jonathan Tah",
+  "Lois Openda",
+  "Mike Maignan",
+  "Alessandro Bastoni",
+  "Nicolo Barella",
+  "Dusan Vlahovic",
+  "Antoine Griezmann",
+  "Mikel Oyarzabal",
+  "Takefusa Kubo",
+  "Aurelien Tchouameni",
+  "Eduardo Camavinga",
+  "Warren Zaire-Emery",
+  "Sergio Busquets",
+  "Luis Suarez",
+  "Jordi Alba",
+  "Karim Benzema",
+  "Neymar",
+  "Sadio Mane",
+  "Riyad Mahrez",
+  "Aleksandar Mitrovic",
+  "Crysencio Summerville",
+  "Callum O'Hare",
+  "Viktor Gyokeres",
+  "Gustavo Hamer",
+  "Endrick",
+  "Pau Cubarsi",
+  "Arda Guler",
+  "Franco Mastantuono",
+  "Emiliano Martinez",
+  "Yann Sommer",
+  "Jan Oblak",
+  "Marc-Andre ter Stegen",
+  "Ederson"
+];
